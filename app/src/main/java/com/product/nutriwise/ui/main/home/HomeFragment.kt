8aea61e25b82decbfa993cbcb71bd155679cf87e
fchzo08 <fachrulghifari04@gmail.com>
2024-06-13 23:11:20 +0700

    update fragment home -> progres indicator

--- app/src/main/java/com/product/nutriwise/ui/main/home/HomeFragment.kt
+++ app/src/main/java/com/product/nutriwise/ui/main/home/HomeFragment.kt
@@ -1,14 +1,18 @@
 package com.product.nutriwise.ui.main.home
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.product.nutriwise.databinding.FragmentHomeBinding
+import kotlin.properties.Delegates
 
 class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
+    private var maxCalorie: Int? = null
+    private var proCalorie: Int? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -20,6 +24,17 @@ class HomeFragment : Fragment() {
         return binding.root
     }
 
+    @SuppressLint("SetTextI18n")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        maxCalorie = 5000
+        proCalorie = 2500
+
+        binding.tvNumOfCal.setText("$proCalorie/$maxCalorie")
+        binding.piCalorie.max = maxCalorie as Int
+        binding.piCalorie.progress = proCalorie as Int
+    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null

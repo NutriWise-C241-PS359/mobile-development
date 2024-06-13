c7da7579dc3c4e464f6ec34efdc211e17fd6b3ef
Tiara Myristica Maitsa <a008d4kx3432@bangkit.academy>
2024-06-13 20:40:13 +0700

    add binding webView

--- app/src/main/java/com/product/nutriwise/ui/webView/WebViewActivity.kt
+++ app/src/main/java/com/product/nutriwise/ui/webView/WebViewActivity.kt
@@ -3,8 +3,10 @@ package com.product.nutriwise.ui.webView
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.product.nutriwise.R
+import com.product.nutriwise.databinding.ActivityWebViewBinding
 
 class WebViewActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityWebViewBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web_view)

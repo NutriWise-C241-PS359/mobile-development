8720765eca727686cfeda20232352a0491d74a29
fchzo08 <fachrulghifari04@gmail.com>
2024-06-13 13:54:22 +0700

    add webview

---
+++ app/src/main/java/com/product/nutriwise/ui/webView/WebViewActivity.kt
@@ -0,0 +1,12 @@
+package com.product.nutriwise.ui.webView
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.product.nutriwise.R
+
+class WebViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_web_view)
+    }
+}

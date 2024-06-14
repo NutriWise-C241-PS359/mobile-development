0a1eabaa79bdd7592a72a1b2128e3376b64c881a
Tiara Myristica Maitsa <a008d4kx3432@bangkit.academy>
2024-06-14 18:34:46 +0700

    update history#2 by tiara

---
+++ app/src/main/java/com/product/nutriwise/ui/main/history/BreakfastActivity.kt
@@ -0,0 +1,15 @@
+package com.product.nutriwise.ui.main.history
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.product.nutriwise.R
+
+class BreakfastActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_breakfast)
+
+        supportActionBar?.setTitle("Sarapan")
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
---
+++ app/src/main/java/com/product/nutriwise/ui/main/history/HistoryDetailActivity.kt
@@ -0,0 +1,46 @@
+package com.product.nutriwise.ui.main.history
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.product.nutriwise.R
+
+class HistoryDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_history_detail)
+
+        supportActionBar?.setTitle("DD/MM/YYYY")
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_history, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.option_breakfast -> {
+                val intent = Intent(this, BreakfastActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            R.id.option_lunch -> {
+                val intent = Intent(this, LunchActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            R.id.option_dinner -> {
+                val intent = Intent(this, DinnerActivity::class.java)
+                startActivity(intent)
+                true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
